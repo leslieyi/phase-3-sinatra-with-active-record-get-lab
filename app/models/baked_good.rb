fd895664f758c4046a1e8bd84b2f960efd04fc22
leslieyi <leslieyi411@gmail.com>
2021-08-22 20:06:56 -0400

    I get it

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,12 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery 
+  # scope :by_price, -> { order(price: :desc) }
+  def self.by_price
+    all.order(price: :desc)
+  end
+
+  def self.most_expensive
+    all.order(price: :desc).first
+  end
 end
